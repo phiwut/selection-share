9c01a5a2c09fff99f41cc084e5de4e8d94ed3603
Philipp Wüthrich <philipp.wuethrich@bbcag.ch>
2016-01-12 14:22:50 +0100

    Added append to highlighter

--- jQuery.highlighter.js
+++ jQuery.highlighter.js
@@ -1,6 +1,8 @@
 /* global jQuery */
 
 $(document).ready(function () {	
+	$("body").append("<script>var clipboard = new Clipboard('#copySelectionShare');</script>");
+
     $("html").highlighter({ "selector": ".holder" });
 
     $('.holder').mousedown(function () {
