6f5d4e4f0c907265b711e49c9b62cc90f6cfcfeb
Philipp Wüthrich <philipp.wuethrich@bbcag.ch>
2016-01-13 22:05:48 +0100

    added append and style

--- jQuery.highlighter.js
+++ jQuery.highlighter.js
@@ -1,6 +1,16 @@
 /* global jQuery */
 
 $(document).ready(function () {
+    
+    $( "body" ).append( "<span class='holder'> \
+<div class='share-highlight-btn'> \
+<div class='btn-caret'></div> \
+<div class='selectionShareBtn'><a id=\"copySelectionShare\" data-clipboard-text=\"copiedText\" title=\"copy\"><img src=\"images/copy.png\" alt=\"copy\" height=\"24\" width=\"24\"></a></div> \
+<div class='selectionShareBtn'><a target=\"_top\" id=\"emailSelectionShare\" title=\"email\"><img src=\"images/email.png\" alt=\"email\" height=\"24\" width=\"24\"></a></div> \
+<div class=\'selectionShareBtn\'><a target=\"_blank\" id=\"twitterSelectionShare\" title=\"tweet\"><img src=\"images/twitter.png\" alt=\"twitter\" height=\"24\" width=\"24\"></a></div> \
+</div> \
+</span> \
+    " );
 
     $("html").highlighter({ "selector": ".holder" });
 
@@ -45,7 +55,7 @@ $(document).ready(function () {
         $("#twitterSelectionShare").attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(selText.trim()) + '%20-%20' + window.location.protocol + "//" + window.location.host + "/")
              .click();
     });
-});
+});
 (function ($) {
     /*
      * Code for triple click from
